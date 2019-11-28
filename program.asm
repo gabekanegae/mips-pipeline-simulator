233c6e65e6f385c4a0f2c9c33203197b7279116b
Gabriel Kanegae <gabrielksouza2009@hotmail.com>
2019-11-27 23:07:51 -0300

    Update example program and add its output

--- program.asm
+++ program.asm
@@ -1,10 +1,25 @@
-lw $t0, 0($sp)
-sw $t1, 0($sp)
-# Comment 1
-add $t2, $t3, $t4 # Comment 2
-sw $t0, 1($t1)
+# Load some data into memory
+addi $t0, $zero, 15     # $t0 = 0xF
+sw $t0, 0($zero)        # Copy $t0 to memory[0]
+addi $t1, $zero, 240    # $t1 = 0xFF
+sw $t1, 4($zero)        # Copy $t1 to memory[4]
 
- # Comment 3
-sll $zero, $zero, 0
-srl $s1, $s2, 4
-sll $t7, $t8, 12
+# Do some calculations
+# memory[8] = 0xFF * (0xF + 0xFF)
+
+add $t3, $t0, $t1       # $t3 = $t0 + $t1
+lw $t4, 4($zero)        # Copy memory[4] to $t4
+mult $t5, $t4, $t3      # $t5 = $t4 * $t3
+sw $t5, 8($zero)        # Copy $t5 to memory[8]
+
+# Verify that the results are correct
+# memory[8] = 240*(240+15) = 61200 = 0xEF10
+
+lw $s0, 8($zero)  
+addi, $s1, $zero, 61200
+addi $s7, $zero, 1
+beq $s0, $s1, 1
+addi $s7, $s7, 1
+
+# If correct, $s7 == 1.
+# If not correct, $s7 == 2.
