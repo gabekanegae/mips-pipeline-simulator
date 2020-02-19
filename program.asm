6bd53c74d2a716ad3ee4473da37a77cdd3749519
Gabriel Kanegae <gabrielksouza2009@hotmail.com>
2020-02-19 15:19:27 -0300

    Fix hex

--- program.asm
+++ program.asm
@@ -1,11 +1,11 @@
 # Load some data into memory
 addi $t0, $zero, 15     # $t0 = 0xF
 sw $t0, 0($zero)        # Copy $t0 to memory[0]
-addi $t1, $zero, 240    # $t1 = 0xFF
+addi $t1, $zero, 240    # $t1 = 0xF0
 sw $t1, 4($zero)        # Copy $t1 to memory[4]
 
 # Do some calculations
-# memory[8] = 0xFF * (0xF + 0xFF)
+# memory[8] = 0xF0 * (0xF + 0xF0)
 
 add $t3, $t0, $t1       # $t3 = $t0 + $t1
 lw $t4, 4($zero)        # Copy memory[4] to $t4
@@ -13,7 +13,7 @@ mult $t5, $t4, $t3      # $t5 = $t4 * $t3
 sw $t5, 8($zero)        # Copy $t5 to memory[8]
 
 # Verify that the results are correct
-# memory[8] = 240*(240+15) = 61200 = 0xEF10
+# memory[8] = 240*(15+240) = 61200 = 0xEF10
 
 lw $s0, 8($zero)  
 addi, $s1, $zero, 61200
